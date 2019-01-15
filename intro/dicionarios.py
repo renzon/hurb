514ae76aaebc02c9fc7ffea80a4d45e8c71177bb
renzon <renzon@gmail.com>
2019-01-15 10:39:25 -0200

    Estrutura aninhada dicionario com lista

--- intro/dicionarios.py
+++ intro/dicionarios.py
@@ -7,3 +7,16 @@ print(linguas['pt-br'])
 print(linguas['en-us'])
 if 'es' in linguas:
     print(linguas['es'])
+
+print(linguas.get('es', 'Lingua não disponível'))
+linguas['es'] = 'Espanhol'
+print(linguas.get('es', 'Lingua não disponível'))
+print(linguas.get('pt-br', 'Lingua não disponível'))
+
+sku_dicionario={
+    'omn-2255': ['989354', '7565'],
+    'omn-2256': ['989355', '7566'],
+}
+
+lista_ids = sku_dicionario['omn-2255']
+print(lista_ids[1])
