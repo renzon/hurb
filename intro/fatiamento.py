9cc069854407e2421aef4cd47e2626c542fcf2de
renzon <renzon@gmail.com>
2019-01-16 14:58:31 -0200

    Fatiamento

---
+++ intro/fatiamento.py
@@ -0,0 +1,13 @@
+frutas = 'caju laranja maça cereja tangirina'.split()
+
+print(frutas[0:2])
+print(frutas[:2])
+
+
+print(frutas[2:len(frutas)])
+print(frutas[2:])
+print(frutas[:])
+print(frutas[1:-1:2])
+print(frutas[-1:0])
+print(frutas[:: -1])
+print(frutas)
