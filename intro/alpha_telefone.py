e6d8e195c8d732a4e64c7b0692c15808f4198789
renzon <renzon@gmail.com>
2019-01-18 14:57:39 -0200

    Problema telefone

---
+++ intro/alpha_telefone.py
@@ -0,0 +1,8 @@
+"""
+>>> calcular_telefone('1-HOME-SWEET-HOME')
+'1-4663-79338-4663 '
+>>> calcular_telefone('MY-MISERABLE-JOB')
+'69-647372253-562'
+"""
+
+# http://dojopuzzles.com/problemas/exibe/encontre-o-telefone/
